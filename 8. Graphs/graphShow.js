891fe5e957a16e0fdcc8ae96adccb75b82b403c3
EduardoLizarazu <84466380+EduardoLizarazu@users.noreply.github.com>
2022-01-22 19:44:10 -0400

    graph representation

---
+++ 8. Graphs/graphShow.js
@@ -0,0 +1,53 @@
+/*
+    2 - 0
+   / \
+  1 - 3
+*/
+
+// Edge List - Represent Graphs
+const graph = [
+    [0, 2],
+    [2, 3],
+    [2, 1],
+    [1, 3]
+];
+
+/*
+    2 - 0
+   / \
+  1 - 3
+*/
+
+// Adjacent List - Represent Graphs
+        //      0     1       2       3
+const graph = [[2], [2,3], [0,1,3], [1,2]];
+
+const graph = {
+    0: [2],
+    1: [2,3],
+    2: [0,1,3],
+    3: [1,2]
+};
+
+/*
+    2 - 0
+   / \
+  1 - 3
+*/
+
+// Adjacent Matrix 
+// Like binary form
+const graph = [
+// = 0  1  2  3
+    [0, 0, 1, 0], // work with 0
+    [0, 0, 1, 1], // work with 1
+    [1, 1, 0, 1], // work with 2
+    [0, 1, 1, 0], // work with 3
+]
+
+const graph = {
+    0: [0, 0, 1, 0], // work with 0
+    1: [0, 0, 1, 1], // work with 1
+    2: [1, 1, 0, 1], // work with 2
+    3: [0, 1, 1, 0], // work with 3
+}
